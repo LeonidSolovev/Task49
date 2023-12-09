e87117bb5b9d73d404c2c518cabcbc5ef97be188
Leonid Solovev <ls3112@yandex.ru>
2023-12-09 22:03:47 +0300

    write_file

--- Task49.py
+++ Task49.py
@@ -32,4 +32,12 @@ def read_file(file_name):
     with open(file_name, "r", encoding='utf-8') as data:
         f_reader = DictReader(data)
         return list(f_reader)
-    
+
+def write_file(file_name, lst):
+    res = read_file(file_name)
+    obj = {"Имя": lst[0], "Фамилия": lst[1], "Телефон": lst[2]}
+    res.append(obj)
+    with open(file_name, "w", encoding='utf-8') as data:
+        f_writer = DictWriter(data, fieldnames=['Имя', 'Фамилия', 'Телефон'])
+        f_writer.writeheader()
+        f_writer.writerows(res)
