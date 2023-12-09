0be532ca59e8bea88b48c55f90b54e174464ba0a
Leonid Solovev <ls3112@yandex.ru>
2023-12-09 22:29:07 +0300

    it works!!!

--- Task49.py
+++ Task49.py
@@ -16,7 +16,6 @@
 from os.path import exists
 from csv import DictReader, DictWriter
 
-
 def get_info():
     first_name = "Иван"
     last_name = "Иванов"
@@ -34,32 +33,34 @@ def create_file(file_name):
 def read_file(file_name):
     with open(file_name, "r", encoding='utf-8') as data:
         f_reader = DictReader(data)
-    return list(f_reader)
+        return list(f_reader)
 
 
 def write_file(file_name, lst):
     res = read_file(file_name)
     obj = {"Имя": lst[0], "Фамилия": lst[1], "Телефон": lst[2]}
     res.append(obj)
-    with open(file_name, "w", encoding='utf-8') as data:
+    with open(file_name, "w", encoding='utf-8', newline='') as data:
         f_writer = DictWriter(data, fieldnames=['Имя', 'Фамилия', 'Телефон'])
         f_writer.writeheader()
         f_writer.writerows(res)
 
+
 file_name = 'phone.csv'
+
 
 def main():
     while True:
-        command = input('Введите команду: ')
+        command = input("Введите команду: ")
         if command == 'q':
             break
         elif command == 'w':
             if not exists(file_name):
                 create_file(file_name)
-            write_file(file_name, get_info) 
+            write_file(file_name, get_info())
         elif command == 'r':
             if not exists(file_name):
-                print('Файл отсутствует, создайте его!')
+                print("Файл отсутствует. Создайте его")
                 continue
             print(*read_file(file_name))
 
